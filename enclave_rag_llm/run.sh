72497e65d77d5e8d2853a4ae1f176bb35af95bbc
Evan Gronberg <evangronberg@Evans-MBP.attlocal.net>
2024-03-02 14:44:51 -0800

    Another Enclave Docker image fix

--- enclave_rag_llm/run.sh
+++ enclave_rag_llm/run.sh
@@ -2,7 +2,7 @@
 
 ifconfig lo 127.0.0.1
 
-echo "127.0.0.1 . kms.us-west-1.amazonaws.com" >> /etc/hosts
+echo "127.0.0.1   kms.us-west-1.amazonaws.com" >> /etc/hosts
 
 nohup python3 /app/traffic_forwarder.py --local-port 443 --remote-cid 3 --remote-port 8000 &
-python3 /app/server.py
+python3 /app/llm_app.py
